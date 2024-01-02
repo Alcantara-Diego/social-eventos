79be962ab7765c1c8e64f389d24145f5e81794d1
Diego Alcântara <ddag0309@gmail.com>
2024-01-02 15:22:07 -0300

    Criado os componentes nav e siderbar, e adicionado sass ao projeto

---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import './style/nav.scss'
+
+function Nav() {
+    return (
+        <nav>
+            <h1 className="logo">SOCIAL-EVENTOS</h1>
+            <div>login</div>
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,13 @@
+import './style/sidebar.scss'
+
+function Sidebar () {
+    return (
+        <ul className="sidebar">
+            <li>Criar evento</li>
+            <li>Ver meus eventos</li>
+            <li>Sobre Nós</li>
+        </ul>
+    )
+}
+
+export default Sidebar;
