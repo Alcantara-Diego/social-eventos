eae2f8bfc02a207cce7834245e5740b10a044da3
Diego Alcântara <ddag0309@gmail.com>
2024-01-11 11:25:15 -0300

    Adicionado subcategorias nos modelos de posts criados

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,23 +1,88 @@
 import './style/feed.scss'
+import { FaCalendar, FaLink} from 'react-icons/fa';
+import { IoIosWarning } from "react-icons/io";
+import { IoGameController } from "react-icons/io5";
+import { IoIosPeople } from "react-icons/io";
+
+
 
 function feed(){
     return(
         <main>
-            <div className="cardContainer">
+            <div className="postsContainer">
+
+
                 <div className="post">
-                    <div className="capa"></div>
+                    
+                    <img src=".\src\assets\postsImg\show1.jpg" alt="" className='capa'/>
                     <div className="conteudo">
                         <header>
-                            <h3 className="titulo">Cena 2k23</h3>
-                            <h6 className="subtitulo">Festival músical</h6>
+                            <h3 className="titulo">Cena 2k</h3>
+                            <h6 className="subtitulo">Festival musical</h6>
+
+                            <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dicta cupiditate, quibusdam maiores ipsa sit at fugit! Soluta nulla dicta ea expedita fugiat, in a explicabo quos. Voluptates, hic! Enim?</p>
+
+                            <div className='extra-info'>
+
+                                <a href=""><FaLink/>Ingressos aqui</a>
+
+                                <div><FaCalendar  /> 12/04/2024</div>
+
+                                <div><IoIosWarning /> Maior de 16 anos</div>
+
+                            </div>
+                            
+
                         </header>
 
-                        <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dicta cupiditate, quibusdam maiores ipsa sit at fugit! Soluta nulla dicta ea expedita fugiat, in a explicabo quos. Voluptates, hic! Enim?</p>
+                        
+                        <div className="interacao">
+                            <p className="confirmadosNumero"> <IoIosPeople />
+                            15 pessoas interessadas</p>
+                            <div className="btnDiv">
+                                <button className="confirmarPresencaBtn">Tenho interesse!</button>
+                                <button className="comentariosBtn">Comentarios</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+
+                <div className="post">
+                    
+                    <img src=".\src\assets\postsImg\gta6.webp" alt="" className='capa'/>
+                    <div className="conteudo">
+                        <header>
+                            <h3 className="titulo">GTA VI</h3>
+                            <h6 className="subtitulo">Lançamento jogo eletrônico</h6>
+
+                            <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dicta cupiditate, quibusdam maiores ipsa sit at fugit! Soluta nulla dicta ea expedita fugiat, in a explicabo quos. Voluptates, hic! Enim?</p>
+
+
+                            <div className='extra-info'>
+
+                                <a href="https://www.youtube.com/watch?v=QdBZY2fkU-0"
+                                
+                                rel="noreferrer"
+                                target="_blank"  ><FaLink/>Trailer</a>
+
+                                <div><FaCalendar  /> Lançamento 2025</div>
+
+                                <div><IoGameController /> Playstation / Xbox / PC</div>
+
+                            </div>
+
+                        </header>
+
+                        
 
                         <div className="interacao">
-                            <p className="confirmadosNumero">15 pessoas confirmadas</p>
+                            <p className="confirmadosNumero"><IoIosPeople />
+                            15 pessoas interessadas</p>
                             <div className="btnDiv">
-                                <button className="confirmarPresencaBtn">Confirmar!</button>
+                                <button className="confirmarPresencaBtn">Tenho interesse!</button>
                                 <button className="comentariosBtn">Comentarios</button>
                             </div>
                         </div>
